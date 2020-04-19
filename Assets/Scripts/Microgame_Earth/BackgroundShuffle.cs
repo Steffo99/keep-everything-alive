94b11423435900d4a0965c1303242dcdf4080649
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-04-19 16:38:25 +0200

    Shuffle some files around

---
+++ Assets/Scripts/Microgame_Earth/BackgroundShuffle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BackgroundShuffle : MonoBehaviour
+{
+    public List<Sprite> backgrounds;
+    
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start() {
+        spriteRenderer.sprite = backgrounds.PickRandom();
+    }
+}
