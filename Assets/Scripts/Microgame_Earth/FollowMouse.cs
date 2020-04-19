8b048637b140522029b9a15a0f1b12be4ba8def0
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-04-19 18:26:30 +0200

    Disallow cheesing

--- Assets/Scripts/Microgame_Earth/FollowMouse.cs
+++ Assets/Scripts/Microgame_Earth/FollowMouse.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class FollowMouse : MonoBehaviour
 {
+    public Vector2 min;
+    public Vector2 max;
+
     private new Rigidbody2D rigidbody2D;
 
     void Awake() {
@@ -13,12 +16,12 @@ public class FollowMouse : MonoBehaviour
 
     void Start() {
         Vector3 screenPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(screenPoint.x, screenPoint.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(screenPoint.x, min.x, max.x), Mathf.Clamp(screenPoint.y, min.y, max.y), transform.position.z);
     }
 
     void FixedUpdate()
     {
         Vector3 screenPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        rigidbody2D.MovePosition(new Vector3(screenPoint.x, screenPoint.y, transform.position.z));
+        rigidbody2D.MovePosition(new Vector3(Mathf.Clamp(screenPoint.x, min.x, max.x), Mathf.Clamp(screenPoint.y, min.y, max.y), transform.position.z));
     }
 }
