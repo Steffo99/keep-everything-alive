e01b03757046f04cd132127516dfa54c9c12e872
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-04-19 16:24:38 +0200

    Add YOUR SHIP microgame

---
+++ Assets/Scripts/BleachBeach/FailOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailOnCollision : MonoBehaviour
+{
+    private MicrogameController microgameController;
+
+    void Awake()
+    {
+        microgameController = GameObject.FindGameObjectWithTag("MicrogameController").GetComponent<MicrogameController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Enemy") {
+            microgameController.victory = false;
+        }
+    }
+}
--- Assets/Scripts/Microgame/MicrogameController.cs
+++ Assets/Scripts/Microgame/MicrogameController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class MicrogameController : MonoBehaviour
+public class MicrogameController : MonoBehaviour
 {
     [Header("Microgame Settings")]
     [BeforeStart]
